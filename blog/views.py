060d1a5df042ffd916901a9cd1ffa47cd44b5d82
Santiago Diaz <zantiago29@hotmail.com>
2017-07-11 01:46:05 +0000

    adding extensions to blog

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,12 @@ from flask import request, redirect, url_for
 from . import app
 from .database import session, Entry
 
+# Mistune - markdown parser for submitting formatted blog posts ##ask for clarification
+import mistune
+
+# Html2Text - convert html to markdown ##ask for clarification
+import html2text
+
 PAGINATE_BY = 10
 
 @app.route("/")
@@ -46,3 +52,44 @@ def add_entry_post():
     session.add(entry)
     session.commit()
     return redirect(url_for("entries"))
+    
+# View single entry
+@app.route("/entry/<int:id>")
+def single_entry(id=1):
+    entries = session.query(Entry)
+    entries = entries.filter(Entry.id == id).all()
+    return render_template("entries.html",
+        entries=entries)
+        
+# GET request for editing existing entry
+@app.route("/entry/<int:id>/edit", methods=["GET"])
+def edit_entry_get(id=1):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id == id).first()
+    return render_template("edit_entry.html", entry_title=entry.title, entry_content=html2text.html2text(entry.content))
+        
+# POST request for editing existing entry
+@app.route("/entry/<int:id>/edit", methods=["POST"])
+def edit_post_post(id=1):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id == id).first()
+    entry.title=request.form["title"]
+    entry.content=mistune.markdown(request.form["content"])
+    session.commit()
+    return redirect(url_for("entries"))
+
+# GET request for deleting existing post
+@app.route("/entry/<int:id>/delete", methods=["GET"])
+def delete_entry_get(id=1):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id == id).first()
+    return render_template("delete_entry.html", entry_title=entry.title)
+
+# POST request for deleting existing post
+@app.route("/entry/<int:id>/delete", methods=["POST"])
+def delete_post_delete(id=1):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id == id).first()
+    session.delete(entry)
+    session.commit()
+    return redirect(url_for("entries"))      
